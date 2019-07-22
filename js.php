1a447f4f1d07611e0c372850ac73c15d13d6e32d
MPewny <41195328+MPewny@users.noreply.github.com>
2019-07-22 13:45:06 +0200

    Create js.php

---
+++ js.php
@@ -0,0 +1,27 @@
+<?php
+function js_alert($alert)
+{
+echo "<script> alert(".$alert."); </script>";
+}
+
+function js_confirm($x,$yes,$no)
+{
+echo "<script> if(confirm(".$x.")){".$yes.";}else{".$no.";} </script>";
+}
+function js_cookie($cookie_value)
+{
+echo '<script> document.cookie = "'.$cookie_value.'"; </script>';
+}
+function js_console-log($log)
+{
+echo '<script>console.info("'.$log.'"); </script>';
+}
+function js_console-clear()
+{
+echo '<script> console.clear(); </script>';
+}
+function js_console-info($info)
+{
+echo '<script>console.info("'.$info.'"); </script>';
+}
+?>
