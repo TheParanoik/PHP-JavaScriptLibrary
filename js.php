759fa22915c3a45c4ca4bd03fb2b9121231c6e11
MPewny <41195328+MPewny@users.noreply.github.com>
2019-12-03 16:19:13 +0100

    Update js.php
    it's now object baby!

--- js.php
+++ js.php
@@ -1,28 +1,36 @@
 <?php
-function js_alert($alert)
+class js
+{
+function alert($alert)
 {
 echo "<script> alert(".$alert."); </script>";
 }
 
-function js_confirm($x,$yes,$no)
+function confirm($x,$yes,$no)
 {
 if(!isset($no)){$no = "window.history.back();";}
 echo "<script> if(confirm(".$x.")){".$yes.";}else{".$no.";} </script>";
 }
-function js_cookie($cookie_value)
+  
+function cookie($cookie_value)
 {
 echo '<script> document.cookie = "'.$cookie_value.'"; </script>';
 }
-function js_consoleLog($log)
+  
+function consoleLog($log)
 {
 echo '<script>console.info("PHP: '.$log.'"); </script>';
 }
-function js_consoleClear()
+  
+function consoleClear()
 {
 echo '<script> console.clear(); </script>';
 }
-function js_consoleInfo($info)
+  
+function consoleInfo($info)
 {
 echo '<script>console.info("'.$info.'"); </script>';
+}
+ 
 }
 ?>
