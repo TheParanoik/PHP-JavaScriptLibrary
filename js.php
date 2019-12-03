7b084b79f32179824427e766b7a49b151d7ed6bc
MPewny <41195328+MPewny@users.noreply.github.com>
2019-12-03 17:54:07 +0100

    add ajax
    Ajax added (not tested yet)
    soon ajax options will be improved and fixed
    stay tuned & calm

--- js.php
+++ js.php
@@ -8,34 +8,75 @@ function script($code)
   
 function alert($alert)
 {
-echo "alert(".$alert.");";
+return "alert(".$alert.");";
 }
 
 function confirm($x,$yes,$no)
 {
 if(!isset($no)){$no = "window.history.back();";}
-echo "if(confirm(".$x.")){".$yes.";}else{".$no.";}";
+return "if(confirm(".$x.")){".$yes.";}else{".$no.";}";
 }
   
 function cookie($cookie_value)
 {
-echo 'document.cookie = "'.$cookie_value.'";';
+return 'document.cookie = "'.$cookie_value.'";';
 }
   
 function consoleLog($log)
 {
-echo 'console.info("PHP: '.$log.'");';
+return 'console.info("PHP: '.$log.'");';
 }
   
 function consoleClear()
 {
-echo 'console.clear();';
+return 'console.clear();';
 }
   
 function consoleInfo($info)
 {
-echo 'console.info("'.$info.'");';
+return 'console.info("'.$info.'");';
 }
  
+  //Ajax (DON'T WORK FOR MICROSOFT BROWSERS!)
+  $ajax = array(
+            "Name" => "",
+            "Target" => "",
+            "Type" => "POST",
+            "Async" => true,
+            "Data" => "",
+            "onReadyState" => array('1' => '', '2' => '', '3' => '', 4 => '')
+            );
+  function ajaxConstructor($ajax){
+    if(empty($ajax['Name'])){
+      $ajax['Name'] = "xmlHttp";
+    }
+    if(
+      echo $ajax['Name']." = new XMLHttpRequest();";
+      
+        if(!empty($ajax['onReadyState']['1'])){
+            echo $ajax['Name']."onreadystatechange=function() {
+             if (this.readyState==1 ) {".$ajax['onReadyState']['1']."}
+            }";
+        }
+        if(!empty($ajax['onReadyState']['2'])){
+            echo $ajax['Name']."onreadystatechange=function() {
+             if (this.readyState==2) {".$ajax['onReadyState']['2']."}
+            }";
+        }
+        if(!empty($ajax['onReadyState']['3'])){
+            echo $ajax['Name']."onreadystatechange=function() {
+             if (this.readyState==3 ) {".$ajax['onReadyState']['3']."}
+            }";
+        }
+        if(!empty($ajax['onReadyState']['4'])){
+            echo $ajax['Name']."onreadystatechange=function() {
+             if (this.readyState==4 && this.status==200) {".$ajax['onReadyState']['4']."}
+            }";
+        }
+      
+      echo $ajax['Name'].".open(".$ajax['Type'].",".$ajax['Target'].",".$ajax['Async'].");
+           ".$ajax['Name'].".send(".$ajax['Data']."); ";
+  }
+  
 }
 ?>
